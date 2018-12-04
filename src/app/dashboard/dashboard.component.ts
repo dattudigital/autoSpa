e4db0eaa5ab8b450309fa0342353e72e5f985d9b
dattudigital <rajani@digitalrupay.com>
2018-12-04 14:35:13 +0530

    dashboard1

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  newUser = true;
+  carUser = true;
+  bikeUser = false;
+  commercialUser = false;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  newUserClick() {
+    this.newUser = true;
+  }
+
+  existingUserClick(){
+    this.newUser = false;
+    this.carUser = false;
+    this.bikeUser = false;
+    this.commercialUser = false;
+  }
+
+  carUserClick() {
+    this.carUser = true;
+    this.bikeUser = false;
+    this.commercialUser = false;
+  }
+
+  bikeUserClick() {
+    this.carUser = false;
+    this.bikeUser = true;
+    this.commercialUser = false;
+  }
+
+  commercialUserClick() {
+    this.carUser = false;
+    this.bikeUser = false;
+    this.commercialUser = true;
   }
 
 }
